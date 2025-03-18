4547a315b79a2768421ac4d0022e7149774d11d5
Umair505 <moinuli359@gmail.com>
2025-03-18 21:59:41 +0600

    updated

--- Algorithm Varsity/Binary_insertion.cpp
+++ Algorithm Varsity/Binary_insertion.cpp
@@ -1,51 +1,51 @@
+// ---------*MOINUL ISLAM UMAIR*-----------
+// Binary Insertion ASCENDING*
 #include<bits/stdc++.h>
 using namespace std;
-void printsorted(vector<int>s)
+void print(vector<int>v)
 {
-    for(int i=0;i<s.size();i++)
+    for(int i =0;i<v.size();i++)
     {
-        cout<<s[i]<<" ";
+        cout<<v[i]<<" ";
     }
-    cout<<endl;
 }
-int main()
+void binary_insertion(vector<int>&u)
 {
-    vector<int>u= {5,10,1,3,2};
-    printsorted(u);
     vector<int>s;
-    int l,h,mid;
     s.push_back(u[0]);
-    for(int i=1; i<5; i++)
+    int l,r,mid;
+    for(int i=1;i<u.size();i++)
     {
-        l=0;
-        h=s.size()-1;
+        l = 0,r=s.size();
         while(1)
         {
-            if(l>h)
+            if(l>r)
             {
-                if(u[i]>s[mid])
+                if(s[mid]<u[i])
                 {
                     s.insert(s.begin()+mid+1,u[i]);
-                    break;
                 }
                 else
                 {
                     s.insert(s.begin()+mid,u[i]);
-                    break;
                 }
+                break;
             }
-            mid=(l+h)/2;
+            mid = (l+r)/2;
             if(u[i]>s[mid])
             {
-                l=mid+1;
+                l = mid + 1;
             }
             if(u[i]<s[mid])
             {
-                h=mid-1;
+                r = mid - 1;
             }
         }
     }
-    cout<<"Final Array"<<endl;
-    printsorted(s);
+    print(s);
 }
- 
+int main()
+{
+    vector<int>u ={20000,-10,1,3,2,-1,-2,-3,-1000};
+    binary_insertion(u);
+}
---
+++ Algorithm Varsity/prac.cpp
@@ -0,0 +1,17 @@
+#include<bits/stdc++.h>
+using namespace std;
+void print(vector<int>v){
+
+}
+void binary_insertion(vector<int>u)
+{
+    int l,r,mid;
+    vector<int>s;
+    s.push_back(u[0]);
+}
+int main()
+{
+    vector<int>u ={5,-10,1,3,2,-1,-2,-3,-1000};
+    binary_insertion(u); 
+    return 0;
+}
